732afbba8805183c09d79091f2754a9474fc4710
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-07-25 15:04:44 -0500

    settings react navigation

--- app/screens/MyAccount.js
+++ app/screens/MyAccount.js
@@ -2,12 +2,11 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 const MyAccount = () => {
-    return (
-        <View>
-          <Text>Estamos en mi cuenta</Text>
-        </View>
-      );
-    };
+  return (
+    <View>
+      <Text>Estamos en mi cuenta</Text>
+    </View>
+  );
 };
 
 export default MyAccount;
--- app/screens/Search.js
+++ app/screens/Search.js
@@ -2,12 +2,11 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 const Search = () => {
-    return (
-        <View>
-          <Text>Estamos en el buscador</Text>
-        </View>
-      );
-    };
+  return (
+    <View>
+      <Text>Estamos en el buscador</Text>
+    </View>
+  );
 };
 
 export default Search;
