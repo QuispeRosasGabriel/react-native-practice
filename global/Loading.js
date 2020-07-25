c7f4f87d5cd4f9afd694e1d994ccdaa66113bba2
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-07-24 20:13:40 -0500

    start

---
+++ global/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Loading = ({user}) => {
+  return (
+    <View>
+      <Text>Loading {user}</Text>
+    </View>
+  );
+};
+
+export default Loading;
